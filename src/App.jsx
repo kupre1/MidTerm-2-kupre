0dbfa3af37d12a36153bda2acb06caf758097b9e
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-10-07 22:19:17 +0400

    todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [inputvalue, setValue] = useState("");
+  const [word, setWord] = useState([]);
+
+  const addword = (event) => {
+    event.preventDefault();
+    if (inputvalue) {
+      setWord([...word, inputvalue]);
+      setValue("");
+    }
+  };
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      <form onSubmit={addword}>
+        <input
+          type="text"
+          value={inputvalue}
+          onChange={(prev) => setValue(prev.target.value)}
+          className=""
+          placeholder="chawere sityva"
+        />
+        <button type="submit"> damateba</button>
+      </form>
+      <div>
+        <ul>
+          {word.map((word, index) => (
+            <li key={index}> {word}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
