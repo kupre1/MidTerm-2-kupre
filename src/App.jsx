a1fb11c7c71e04cb951c3f5de3355e7804b2e0d3
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-10-08 19:32:15 +0400

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://randomuser.me/api/?results=10"
+        );
+        setUsers(response.data.results);
+      } catch (error) {}
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      <div>
+        {users.map((user, index) => (
+          <div key={index} style={styles.card}>
+            <img
+              src={user.picture.large}
+              alt={`${user.name.first} ${user.name.last}`}
+              style={styles.image}
+            />
+            <h3>{`${user.name.first} ${user.name.last}`}</h3>
+            <p>{user.email}</p>
+            <p>
+              {user.location.city}, {user.location.country}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
